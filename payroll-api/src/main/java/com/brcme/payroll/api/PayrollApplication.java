5f0ea9c64afdf5986df7fa829bfb4664632f89f1
Pedro Rabis <rabis.pedro@gmail.com>
2025-01-20 07:31:22 -0300

    feat(payroll-api): upgrade rest templato to feign client

--- payroll-api/src/main/java/com/brcme/payroll/api/PayrollApplication.java
+++ payroll-api/src/main/java/com/brcme/payroll/api/PayrollApplication.java
@@ -2,7 +2,9 @@ package com.brcme.payroll.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PayrollApplication {
 	public static void main(String[] args) {
--- payroll-api/src/main/java/com/brcme/payroll/api/services/PaymentService.java
+++ payroll-api/src/main/java/com/brcme/payroll/api/services/PaymentService.java
@@ -1,15 +1,12 @@
 package com.brcme.payroll.api.services;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.brcme.payroll.api.entities.Payment;
 import com.brcme.payroll.api.entities.Worker;
+import com.brcme.payroll.api.repositories.WorkerRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -18,17 +15,10 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 @Service
 public class PaymentService {
-	@Value("${worker-api.host}")
-	private String workerApiHost;
-
-	private final RestTemplate workerApiRestTemplate;
+	private final WorkerRepository workerRepository;
 
 	public Payment getPayment(UUID workerId, Integer days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", workerId.toString());
-		
-		Worker worker = workerApiRestTemplate
-		.getForObject(workerApiHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerRepository.getById(workerId).getBody();
 		log.info("WorkerAPI Response {}", worker);
 		
 		return Payment
