4228378c6857a87ef918f7db77e118625be0e4d8
Pedro Rabis <rabis.pedro@gmail.com>
2025-01-24 18:18:37 -0300

    feat(eureka-server): add eureka clients

--- payroll-api/src/main/java/com/brcme/payroll/api/PayrollApplication.java
+++ payroll-api/src/main/java/com/brcme/payroll/api/PayrollApplication.java
@@ -2,10 +2,10 @@ package com.brcme.payroll.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@RibbonClient(name = "worker-api-client")
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class PayrollApplication {
--- worker-api/src/main/java/com/brcme/worker/api/WorkerApplication.java
+++ worker-api/src/main/java/com/brcme/worker/api/WorkerApplication.java
@@ -2,7 +2,9 @@ package com.brcme.worker.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WorkerApplication {
 	public static void main(String[] args) {
